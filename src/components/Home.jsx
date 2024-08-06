ed503bac7936be5be8bd169a016ed92b88148f66
Paras Wadkar <119865515+ParasWadkarhe@users.noreply.github.com>
2024-08-06 14:35:51 +0000

    basic folder structure

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Home.css';
+
+const Home = () => {
+    return (
+        <>
+            <p className='demo'>Hello</p>
+        </>
+    )
+}
+
+export default Home;
