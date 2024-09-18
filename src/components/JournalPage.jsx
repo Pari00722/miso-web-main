24f74738cab39df53d8990591cc2228283185e81
Simarjeet Singh <162547777+Simarjeet2208@users.noreply.github.com>
2024-09-18 22:12:22 +0530

    Update JournalPage.jsx

--- src/components/JournalPage.jsx
+++ src/components/JournalPage.jsx
@@ -8,66 +8,65 @@ const JournalPage = () => {
   const { category } = useParams();
 
   return (
-    <div className='journalPageMainParent-1'>
-      <div className='journalPageMainParent max-w-4xl mx-auto px-4'>
-        <p className='sjText'> THE GIFTING JOURNAL </p>
-
-        <div className='paraHold absolute'>
-          <p className='flex font-times text-xl text-justify'>
-            Discover creative ideas and inspiration for choosing the perfect gifts for every occasion. From unique gift guides and DIY projects to stories behind our handcrafted products, our journal is your go-to source for all things gifting.
-          </p>
-        </div>
-
-        <p className='fof text-3xl text-center absolute aprilBlogHeading font-times'>
-          {category === 'gift-guides' ? 'Gifting Guides: Ideas for Every Occasion' : 'DIY Projects: Craft Your Own Unique Gifts'}
+    <div className='container'>
+      <div className='sjText'> THE GIFTING JOURNAL </div>
+      <div className='paraHold'>
+        <p className='flex font-times text-xl text-justify'>
+          Discover creative ideas and inspiration for choosing the perfect gifts for every occasion. From unique gift guides and DIY projects to stories behind our handcrafted products, our journal is your go-to source for all things gifting.
         </p>
+      </div>
 
-        <div className='hairImgHold'>
-          <img src={category === 'gift-guides' ? '/assets/journalimg1.png' : '/assets/journalimg2.png'} className="hairImg" alt='blog' />
-        </div>
+      <p className='fof mt-20 text-3xl text-center aprilBlogHeading font-times'>
+          {category === 'gift-guides' ? 'Gifting Guides: Ideas for Every Occasion' : 'DIY Projects: Craft Your Own Unique Gifts'}
+      </p>
 
-        <div className='jhold flex flex-col absolute'>
-          {category === 'gift-guides' ? (
-            <>
-              <h2 className='font-times ml-9 text-center text-2xl'>
-              <br/>
-              <strong> Ultimate Gifting Guide:</strong></h2> <br/>
-                <p className= "font-times text-xl text-justify mt-1">Find the Perfect Personalized Wooden Engraved Gifts:<br/> Personal gifts have a way of touching hearts at Miso. That is why our collection of wooden products, which are all personally done and engraved, make any occasion extraordinary. Be it birthdays, anniversaries, or holidays, our gifting guide will help you find a meaningful present that shows you truly care.<br/><br/>
-              </p>
-              <h2 className='text-2xl font-bold mt-4 font-times'>Gifting for Every Occasion</h2>
-              <ul className='list-disc ml-5 mt-2 text-justify font-times text-xl'>
-                <li><strong>Birthdays:</strong> Give your loved ones uniquely crafted wooden jewelry boxes or picture frames with laser engravings that capture their most treasured moments.</li><br/>
-                <li><strong>Anniversaries:</strong> Make this special day with custom-engraved wine boxes or beautifully created keepsakes.</li><br/>
-                <li><strong>Weddings:</strong> Celebrate love with custom-engraved wood coasters or photo albums, or custom signage for the big day.</li><br/>
-                <li><strong>Corporate Gifts:</strong> Impress the sense with our range of sophisticated wooden desk accessories and plaques, ideal for clients and staff alike.</li><br/><br/>
-              </ul> 
-              <h2 className='text-2xl font-bold mt-4 font-times'>Gift Ideas by Personality</h2>
-              <ul className='list-disc ml-5 mt-2 text-justify font-times text-xl'>
-                <li><strong>For the Minimalist:</strong> Sleek, discreet wooden keychains and sets of pens afford elegance in simplicity.</li><br/>
-                <li><strong>For the Nature Lover:</strong> Wooden planters or serving boards with engravings add a rustic touch, very organic, to any home.</li><br/>
-                <li><strong>For the Sentimental:</strong> Research customized memory boxes in which treasured keepsakes can be stored and preserved over time.</li><br/>
-              </ul>
-
-              <h2 className='text-2xl font-bold mt-4 font-times'>Why Miso?</h2>
-              <div className='list-disc ml-1 mt-2 text-justify font-times text-xl'>
-              <li><strong>Sustainable:</strong> Our products are made from eco-friendly materials so that your gift is as great for the planet as it is for your recipient.</li><br/>
-               
-               
-              <li><strong>Personalization:</strong> We take it a step ahead with the option of personalized engravings so that you can make each gift truly special.</li><br/>
-
-              <li><strong>Handcrafted:</strong> Our skilled artisans meticulously create each Miso gift, infusing it with care and precision, making every piece as unique as your special occasion.</li><br/><br/>
-              </div>
-
-                <h2 className='text-2xl font-bold mt-4 font-times'>Need Inspiration?</h2>
-                <div className='text-xl font-times flex text-justify'>  Shop top selling gift categories or chat with our team for ideas unique to you.<br/>
-                Ready to give the perfect gift? Shop Miso's collection today and give a gift they'll always remember.
-                </div>
-             
+      <div className='hairImgHold'>
+        <img src={category === 'gift-guides' ? '/assets/journalimg1.png' : '/assets/journalimg2.png'} className="hairImg" alt='blog' />
+      </div>
+      
+      <div className="jHold">
+      {category === 'gift-guides' ? (
+        <>
+          <div className="gift-guide-container">
               
-            </>
-          ) : (
+            <h2 className='font-times ml-9 text-center text-2xl'>
+            <br/>
+            <strong> Ultimate Gifting Guide:</strong></h2> <br/>
+            <p className= "font-times text-xl text-justify mt-1">Find the Perfect Personalized Wooden Engraved Gifts:<br/> Personal gifts have a way of touching hearts at Miso. That is why our collection of wooden products, which are all personally done and engraved, make any occasion extraordinary. Be it birthdays, anniversaries, or holidays, our gifting guide will help you find a meaningful present that shows you truly care.<br/><br/></p>
+            <h2 className='text-center text-2xl font-bold mt-4 font-times'>Gifting for Every Occasion</h2>
+            <ul className='list-disc ml-5 mt-2 text-justify font-times text-xl'>
+              <li><strong>Birthdays:</strong> Give your loved ones uniquely crafted wooden jewelry boxes or picture frames with laser engravings that capture their most treasured moments.</li><br/>
+              <li><strong>Anniversaries:</strong> Make this special day with custom-engraved wine boxes or beautifully created keepsakes.</li><br/>
+              <li><strong>Weddings:</strong> Celebrate love with custom-engraved wood coasters or photo albums, or custom signage for the big day.</li><br/>
+              <li><strong>Corporate Gifts:</strong> Impress the sense with our range of sophisticated wooden desk accessories and plaques, ideal for clients and staff alike.</li><br/><br/>
+            </ul> 
+            <h2 className='text-2xl font-bold mt-4 font-times'>Gift Ideas by Personality</h2>
+            <ul className='list-disc ml-5 mt-2 text-justify font-times text-xl'>
+              <li><strong>For the Minimalist:</strong> Sleek, discreet wooden keychains and sets of pens afford elegance in simplicity.</li><br/>
+              <li><strong>For the Nature Lover:</strong> Wooden planters or serving boards with engravings add a rustic touch, very organic, to any home.</li><br/>
+              <li><strong>For the Sentimental:</strong> Research customized memory boxes in which treasured keepsakes can be stored and preserved over time.</li><br/>
+            </ul>
+
+            <h2 className='text-2xl font-bold mt-4 font-times'>Why Miso?</h2>
+            <div className='list-disc ml-1 mt-2 text-justify font-times text-xl'>
+            <li><strong>Sustainable:</strong> Our products are made from eco-friendly materials so that your gift is as great for the planet as it is for your recipient.</li><br/>
+              
+              
+            <li><strong>Personalization:</strong> We take it a step ahead with the option of personalized engravings so that you can make each gift truly special.</li><br/>
+
+            <li><strong>Handcrafted:</strong> Our skilled artisans meticulously create each Miso gift, infusing it with care and precision, making every piece as unique as your special occasion.</li><br/><br/>
+            </div>
+
+            <h2 className='text-2xl font-bold mt-4 font-times'>Need Inspiration?</h2>
+            <div className='text-xl font-times flex text-justify'>  Shop top selling gift categories or chat with our team for ideas unique to you.<br/>
+            Ready to give the perfect gift? Shop Miso's collection today and give a gift they'll always remember.
+            </div>
+          </div>
+        </>
+          ):(
             <>
-             <h2 className='font-times ml-9 text-center text-2xl'>
+             <div className="gift-guide-container">
+              <h2 className='font-times ml-9 text-center text-2xl'>
                 <br/>
                 <strong>Get Creative with Miso:</strong>
               </h2>
@@ -111,29 +110,30 @@ const JournalPage = () => {
               <div className='text-xl font-times flex text-justify'>
                 Ready to dive into your next DIY adventure? Browse our full collection of DIY engraving kits and get crafting today. No matter your skill level, Miso’s kits provide everything you need to create stunning wooden engraved pieces from the comfort of your home.
               </div>
+             </div>
             </>
           )}
-        </div>
+      </div>
 
-        <div className='footerFeatures3 flex flex-row absolute font-times'>
-          <div className='flex-1 text-center'>
+      <div className='footerFeatures3 mt-5 sm:mt-40 flex flex-col sm:flex-row flex-wrap justify-between gap-20 md:gap-20 sm:gap-2 sm:flex-row sm:items-center font-times'>
+        <div className='flex-1 text-center'>
             <FaShippingFast size={32} className="mx-auto mb-2" />
             <p>2 DAY DELIVERY</p>
-          </div>
-          <div className='flex-1 text-center'>
+        </div>
+        <div className='flex-1 text-center'>
             <FaLock size={32} className="mx-auto mb-2" />
             <p>SECURE CHECKOUT</p>
-          </div>
-          <div className='flex-1 text-center'>
+        </div>
+        <div className='flex-1 text-center'>
             <BsCurrencyDollar size={32} className="mx-auto mb-2" />
             <p>ROYALTY POINTS</p>
-          </div>
-          <div className='flex-1 text-center'>
+        </div>
+        <div className='flex-1 text-center'>
             <FaExchangeAlt size={32} className="mx-auto mb-2" />
             <p>EASY RETURNS</p>
-          </div>
         </div>
       </div>
+      
     </div>
   );
 };
