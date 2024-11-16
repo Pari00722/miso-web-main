00136e6f8232aa50222e05a58c5751b7bc8cfe55
Simarjeet Singh <162547777+Simarjeet2208@users.noreply.github.com>
2024-11-16 18:00:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Change output directory to "build"
+    outDir: 'dist', // Ensure this is set to 'dist'
   },
 });
